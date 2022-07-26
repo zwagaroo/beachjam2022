fd9268b61e870acb044b531dab3e66cce2f14fb6
UwU <->
2022-07-25 21:01:21 -0400

    Added naval mine model/basic materials, script which calls explosion

---
+++ Beach Jam 2022/Assets/Prefabs/Naval Mine/NavalMine.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavalMine : MonoBehaviour
+{
+	GameObject explosionPrefab;
+	
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        //Testing Code
+        yield return new WaitForSeconds(3f);
+        Explode();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void Explode()
+	{
+		Instantiate(explosionPrefab, transform.position, transform.rotation);
+		// Create Explosion here
+		Destroy(gameObject);
+	}
+}
