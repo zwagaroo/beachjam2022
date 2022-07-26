e35ef7335e40467403928135d3ed7070a3d946b9
chkohtz <84747967+chkohtz@users.noreply.github.com>
2022-07-25 21:05:27 -0400

    Explosion death and destroy explosion
    Made an explosion prefab and minion explodes and destroys itself after set time when game is started

---
+++ Beach Jam 2022/Assets/ExplosionDeath.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionDeath : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public float timeToDie = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DeathTimer(timeToDie));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DeathTimer(float time)
+    {
+        yield return new WaitForSeconds(time);
+        var explosionObject = Instantiate(explosionPrefab, this.transform.position, this.transform.rotation);
+        //var explosion = explosionObject.GetComponent<ParticleSystem>();
+        Destroy(this.gameObject);
+        //yield return new WaitUntil(() => explosion.isPlaying == false);
+        Debug.Log("explosion done");
+        
+    }
+}
