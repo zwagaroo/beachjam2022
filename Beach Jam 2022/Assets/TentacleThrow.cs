d96666fee6ea2a4a8c147b4ececff1a3b68997df
chkohtz <84747967+chkohtz@users.noreply.github.com>
2022-07-27 01:56:29 -0400

    ball
    ball prefab
    tentaclethrow edited but doesnt spawn ball in corrected place

--- Beach Jam 2022/Assets/TentacleThrow.cs
+++ Beach Jam 2022/Assets/TentacleThrow.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TentacleThrow : MonoBehaviour
 {
     public GameObject ballPrefab;
+    public Camera mCamera;
+    public Transform player;
+    public Canvas projectileCanvas;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +19,12 @@ public class TentacleThrow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 mousepos = mCamera.ScreenToWorldPoint(Input.mousePosition);
+            var newball = Instantiate(ballPrefab, new Vector3(mousepos.x, mousepos.y, mousepos.z), Quaternion.Euler(30,45,0));
+            newball.transform.parent = projectileCanvas.transform;
+
+        }
     }
 }
