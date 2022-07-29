b0bb812dec71e346175c9be2746f82a4c820f6c9
chkohtz <84747967+chkohtz@users.noreply.github.com>
2022-07-29 00:44:43 -0400

    start prefab
    saved start button as prefab
    start game can load any inputted scene

--- Beach Jam 2022/Assets/StartGame.cs
+++ Beach Jam 2022/Assets/StartGame.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-
+    public string sceneToLoad;
     public void LoadScene()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
