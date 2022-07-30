4ef574f3c348e12b6015a6256d5129d419e340d1
zwagaroo <38480808+zwagaroo@users.noreply.github.com>
2022-07-30 00:11:55 -0400

    put audio for ocean scene

--- Beach Jam 2022/Assets/Scripts/AudioManager.cs
+++ Beach Jam 2022/Assets/Scripts/AudioManager.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 public class AudioManager : MonoBehaviour
 {
 
-    public static float globalVolume;
+    public static float globalVolume = 1;
     public Sound[] sounds;
     public static AudioManager instance;
 
@@ -34,15 +34,38 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+<<<<<<< Updated upstream
     public void Play(string name){
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Play();
         currentlyPlaying.Add(s);
+=======
+    void Update(){
+        //volume changes
+        foreach(Sound s in sounds){
+            s.source.volume = globalVolume * s.volume;
+        }
+    }
+
+    IEnumerator Clean(string name){
+            yield return new WaitForSeconds(Array.Find(sounds, sound => sound.name == name).clip.length + 1);
+            foreach(Sound s in currentlyPlaying.ToList()){
+                if(s.source.isPlaying == false){
+                    currentlyPlaying.Remove(s);
+                }
+            }
+    }
+
+    public void Play(string name){
+        Array.Find(sounds, sound => sound.name == name).source.Play();
+/*         currentlyPlaying.Add(s); */
+        StartCoroutine(Clean(name));
+>>>>>>> Stashed changes
     }
 
     public void Stop(string name){
-        currentlyPlaying.Find(s => s.name == name).source.Stop();
-        currentlyPlaying.Remove(currentlyPlaying.Find(s => s.name == name));
+        Array.Find(sounds, s => s.name == name).source.Stop();
+        /* currentlyPlaying.Remove(currentlyPlaying.Find(s => s.name == name)); */
     }
 
     public void Pause(string name){
